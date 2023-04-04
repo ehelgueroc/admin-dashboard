d87861c896330d5e41f917b768a48f8d85fbde95
Esteban Helguero <esteban.helguero@s4rb.com>
2023-04-04 08:07:06 +0100

    Added some comments

--- src/theme.js
+++ src/theme.js
@@ -4,44 +4,45 @@ import { getColorTokens } from './utils/getColorTokens';
 
 // mui theme settings
 export const themeSettings = (mode) => {
+  // get all the variants colors
   const colors = getColorTokens(mode);
+  // set the fontFamily by default
   const fontFamily = ['Source Sans Pro', 'sans-serif'].join(',');
+
+  // set base dark colors
+  const darkModeColors = {
+    primary: {
+      main: colors.primary[500],
+    },
+    secondary: {
+      main: colors.greenAccent[500],
+    },
+    neutral: {
+      dark: colors.grey[700],
+      main: colors.grey[500],
+      light: colors.grey[100],
+    },
+    background: {
+      default: colors.primary[500],
+    },
+  };
+
+  // updated to be light colors
+  const lightModeColors = {
+    ...darkModeColors,
+    primary: {
+      main: colors.primary[100],
+    },
+    background: {
+      default: '#fcfcfc',
+    },
+  };
+
+  // return required custom object for mui to set styles
   return {
     palette: {
       mode,
-      ...(mode === 'dark'
-        ? {
-            primary: {
-              main: colors.primary[500],
-            },
-            secondary: {
-              main: colors.greenAccent[500],
-            },
-            neutral: {
-              dark: colors.grey[700],
-              main: colors.grey[500],
-              light: colors.grey[100],
-            },
-            background: {
-              default: colors.primary[500],
-            },
-          }
-        : {
-            primary: {
-              main: colors.primary[100],
-            },
-            secondary: {
-              main: colors.greenAccent[500],
-            },
-            neutral: {
-              dark: colors.grey[700],
-              main: colors.grey[500],
-              light: colors.grey[100],
-            },
-            background: {
-              default: '#fcfcfc',
-            },
-          }),
+      ...(mode === 'dark' ? darkModeColors : lightModeColors),
       typography: {
         fontFamily,
         fontSize: 12,
@@ -74,10 +75,14 @@ export const themeSettings = (mode) => {
   };
 };
 
+// create a context to share the theme
 export const ColorModeContext = createContext({});
 
+// create the hook
 export const useMode = () => {
+  // set the state for the dark or light color
   const [mode, setMode] = useState('dark');
+  // create a function to allow to change the color
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => setMode((prev) => (prev === 'light' ? 'dark' : 'light')),
@@ -85,6 +90,7 @@ export const useMode = () => {
     [],
   );
 
+  // set a theme variable to get the theme base on the mode
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return [theme, colorMode];
