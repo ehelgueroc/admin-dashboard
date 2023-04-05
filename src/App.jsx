f3e5e4168ea2def6f79a8a7f05e0efa83eba3e4f
Esteban Helguero <esteban.helguero@s4rb.com>
2023-04-05 07:46:06 +0100

    Created new components and added routes to the app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 // ColorModeContext is a context in which we can set the dark or light mode
 // useMode is a hook to get the actual theme and the function to change the color
+import { Route, Routes } from 'react-router-dom';
 import { ColorModeContext, useMode } from './theme';
 // Component to show the top bar
 import TopBar from './scenes/global/TopBar';
+import Dashboard from './scenes/dashboard';
+import Sidebar from './scenes/global/Sidebar';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts';
+import Bar from './scenes/bar';
+import Form from './scenes/form';
+import Line from './scenes/line';
+import Pie from './scenes/pie';
+import FAQ from './scenes/faq';
+import Geography from './scenes/geography';
+import Calendar from './scenes/calendar';
 
 function App() {
   // get from the hook the theme and colorMode
@@ -18,7 +31,21 @@ function App() {
         <div className='app'>
           <main className='content'>
             <TopBar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/calendar' element={<Calendar />} />
+            </Routes>
           </main>
+          <Sidebar />
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
