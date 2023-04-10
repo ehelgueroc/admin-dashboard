debd9ff55465069ef870923c33679390e2f5e0a2
Esteban Helguero <esteban.helguero@s4rb.com>
2023-04-10 06:29:03 +0100

    Added more fields to the form and button

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Formik } from 'formik';
 import * as yup from 'yup';
-import { Box, useTheme, useMediaQuery, TextField } from '@mui/material';
+import { Box, useTheme, useMediaQuery, TextField, Button } from '@mui/material';
 import Header from '../../components/Header';
 import { getColorTokens } from '../../utils/getColorTokens';
 
 function Form() {
   const theme = useTheme();
   const colors = getColorTokens(theme.palette.mode);
-  console.log(colors);
   const isNonMobile = useMediaQuery('(min-width:600px)');
 
   const initialValues = {
@@ -56,7 +55,7 @@ function Form() {
                 fullWidth
                 variant='filled'
                 type='text'
-                label='FirstName'
+                label='First Name'
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.firstName}
@@ -65,6 +64,79 @@ function Form() {
                 helperText={touched.firstName && errors.firstName}
                 sx={{ gridColumn: 'span 2' }}
               />
+              <TextField
+                fullWidth
+                variant='filled'
+                type='text'
+                label='Last Name'
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name='lastName'
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{ gridColumn: 'span 2' }}
+              />
+              <TextField
+                fullWidth
+                variant='filled'
+                type='text'
+                label='Email'
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name='email'
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: 'span 4' }}
+              />
+              <TextField
+                fullWidth
+                variant='filled'
+                type='text'
+                label='Contact'
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contact}
+                name='contact'
+                error={!!touched.contact && !!errors.contact}
+                helperText={touched.contact && errors.contact}
+                sx={{ gridColumn: 'span 4' }}
+              />
+              <TextField
+                fullWidth
+                variant='filled'
+                type='text'
+                label='Address 1'
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address1}
+                name='address1'
+                error={!!touched.address1 && !!errors.address1}
+                helperText={touched.address1 && errors.address1}
+                sx={{ gridColumn: 'span 4' }}
+              />
+              <TextField
+                fullWidth
+                variant='filled'
+                type='text'
+                label='Address 2'
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address2}
+                name='address2'
+                error={!!touched.address2 && !!errors.address2}
+                helperText={touched.address2 && errors.address2}
+                sx={{ gridColumn: 'span 4' }}
+              />
+            </Box>
+            <Box display='flex' justifyContent='end' mt='20px'>
+              <Button
+                type='submit'
+                sx={{ backgroundColor: `${colors.greenAccent[500]}`, color: 'inherit' }}
+              >
+                Create new contact
+              </Button>
             </Box>
           </form>
         )}
