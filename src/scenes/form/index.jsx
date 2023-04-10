af936b387e842e817d3dca6248889206b1ffae80
Esteban Helguero <esteban.helguero@s4rb.com>
2023-04-10 06:17:54 +0100

    WIP Formik contact form

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Formik } from 'formik';
 import * as yup from 'yup';
-import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import { Box, useTheme, useMediaQuery, TextField } from '@mui/material';
 import Header from '../../components/Header';
 import { getColorTokens } from '../../utils/getColorTokens';
 
@@ -37,14 +37,37 @@ function Form() {
   };
 
   return (
-    <Box>
+    <Box m='20px'>
       <Header title='FORM' subtitle='Form to add new data' />
       <Formik
         onSubmit={handleFormSubmit}
         initialValues={initialValues}
         validationSchema={profileSchema}
       >
-        Hola
+        {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display='grid'
+              gap='30px'
+              gridTemplateColumns='repeat(4, minmax(0,1fr))'
+              sx={{ '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' } }}
+            >
+              <TextField
+                fullWidth
+                variant='filled'
+                type='text'
+                label='FirstName'
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName}
+                name='firstName'
+                error={!!touched.firstName && !!errors.firstName}
+                helperText={touched.firstName && errors.firstName}
+                sx={{ gridColumn: 'span 2' }}
+              />
+            </Box>
+          </form>
+        )}
       </Formik>
     </Box>
   );
